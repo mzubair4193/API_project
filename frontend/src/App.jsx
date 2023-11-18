dfc2799b475db3d296fb74b7ac3200bdc56048ec
mzubair <mzubair4193@gmail.com>
2023-11-17 16:27:35 -0800

    ready for deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,43 @@
+import { Outlet, createBrowserRouter, RouterProvider } from "react-router-dom";
+// import LoginFormPage from "./componenets/LoginFormPage/LoginFormPage"
+// import SignupFormPage from "./components/SignUpFormPage/SignUpFormPage";
+import * as sessionActions from './store/session';
+import { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import Navigation from './components/Navigation/Navigation'
+
+function Layout() {
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    dispatch(sessionActions.restoreUser()).then(() => {
+      setIsLoaded(true)
+    });
+  }, [dispatch]);
+
+  return (
+    <>
+      <Navigation isLoaded={isLoaded} />
+      {isLoaded && <Outlet />}
+    </>
+  );
+}
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <h1>Welcome!</h1>
+      }
+    ]
+  }
+]);
+
 function App() {
-  return <h1> Hello from App </h1>;
+  return <RouterProvider router={router} />
 }
 
 export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,3 @@
-// frontend/src/main.jsx
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -7,26 +5,26 @@ import './index.css';
 import { Provider } from 'react-redux';
 import configureStore from './store/store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
+import * as sessionActions from './store/session'
+import { ModalProvider, Modal } from './context/Modal';
 
-
-const store = configureStore();
+const store = configureStore()
 
 if (import.meta.env.MODE !== 'production') {
   restoreCSRF();
 
   window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions
 }
-
-if (process.env.NODE_ENV !== 'production') {
-  window.store = store;
-}
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ModalProvider>
+      <Provider store={store}>
+        <App />
+        <Modal />
+      </Provider>
+    </ModalProvider>
   </React.StrictMode>
 );
