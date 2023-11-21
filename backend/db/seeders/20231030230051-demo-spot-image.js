9d494aedb34405238e2b840519541980f0877d8b
mzubair <mzubair4193@gmail.com>
2023-11-20 21:32:46 -0800

    multiple spot images added

--- backend/db/seeders/20231030230051-demo-spot-image.js
+++ backend/db/seeders/20231030230051-demo-spot-image.js
@@ -32,28 +32,113 @@ module.exports = {
         preview: false
       },
       {
+        spotId: 1,
+        url: 'https://shorturl.at/fsHP8',
+        preview: false
+      },
+      {
+        spotId: 1,
+        url: 'https://shorturl.at/hoDX1',
+        preview: false
+      },
+      {
         spotId: 2,
-        url: 'spotimg2.com',
+        url: 'https://shorturl.at/eJOU0',
+        preview: false
+      },
+      {
+        spotId: 2,
+        url: 'https://shorturl.at/oXY07',
+        preview: false
+      },
+      {
+        spotId: 2,
+        url: 'https://shorturl.at/EJQ24',
+        preview: false
+      },
+      {
+        spotId: 2,
+        url: 'https://tinyurl.com/2yj789pj',
         preview: false
       },
       {
         spotId: 3,
-        url: 'spotimg3.com',
+        url: 'https://tinyurl.com/msnu62c9',
+        preview: false
+      },
+      {
+        spotId: 3,
+        url: 'https://tinyurl.com/uzrkdhte',
+        preview: false
+      },
+      {
+        spotId: 3,
+        url: 'https://tinyurl.com/m8nkhpcp',
+        preview: false
+      },
+      {
+        spotId: 3,
+        url: 'https://tinyurl.com/bdff3st6',
         preview: false
       },
       {
         spotId: 4,
-        url: 'spotimg4.com',
+        url: 'https://tinyurl.com/k69c67z6',
+        preview: false
+      },
+      {
+        spotId: 4,
+        url: 'https://tinyurl.com/zam9ss2d',
+        preview: false
+      },
+      {
+        spotId: 4,
+        url: 'https://tinyurl.com/5uum2apr',
+        preview: false
+      },
+      {
+        spotId: 4,
+        url: 'https://tinyurl.com/37k7f73k',
         preview: false
       },
       {
         spotId: 5,
-        url: 'spotimg5.com',
+        url: 'https://tinyurl.com/2va9ccce',
+        preview: false
+      },
+      {
+        spotId: 5,
+        url: 'https://tinyurl.com/y67hvcux',
+        preview: false
+      },
+      {
+        spotId: 5,
+        url: 'https://tinyurl.com/bdy2eekj',
+        preview: false
+      },
+      {
+        spotId: 5,
+        url: 'https://tinyurl.com/246pcpd5',
         preview: false
       },
       {
         spotId: 6,
-        url: 'spotimg6.com',
+        url: 'https://tinyurl.com/4vs8vr5y',
+        preview: false
+      },
+      {
+        spotId: 6,
+        url: 'https://tinyurl.com/5699drj',
+        preview: false
+      },
+      {
+        spotId: 6,
+        url: 'https://tinyurl.com/2m2dnjd4',
+        preview: false
+      },
+      {
+        spotId: 6,
+        url: 'https://tinyurl.com/5223dfet',
         preview: false
       }
     ], { validate: true })
