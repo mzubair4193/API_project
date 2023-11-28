fa4bce131bc38efbd2f9e0cf71f724e82889b728
mzubair <mzubair4193@gmail.com>
2023-11-28 04:31:31 -0800

    seeders fix

--- backend/db/seeders/20231030224625-demo-spot.js
+++ backend/db/seeders/20231030224625-demo-spot.js
@@ -43,7 +43,7 @@ module.exports = {
         lat: 34.65763,
         lng: 23.35521,
         name: 'The Atlanta House',
-        description: `Surrounded by tall trees and hidden behind an iron gate, this fairytale-esque mansion will make your holiday gathering extra special this year! Featuring 30,000 sq ft of indoor space and 10 spacious bedrooms with en suite bathrooms, this mansion is perfect for big groups & intimate gatherings. Amenities include a heated salt water pool + hot tub, a Chef’s Kitchen, a theater room, a Casablanca style bar + more!`,
+        description: `Surrounded by tall trees and hidden behind an iron gate, this fairytale-esque mansion will make your holiday gathering extra special this year! Featuring 30,000 sq ft of indoor space and 10 spacious bedrooms with en suite bathrooms, this mansion is perfect for big groups & intimate gatherings.`,
         price: 4245
       },
       {
@@ -55,7 +55,7 @@ module.exports = {
         lat: 38.23436,
         lng: 37.35622,
         name: 'The Miami House',
-        description: `Spectacular waterfront villa on Palm Island! This Spanish Colonial style villa features views across Biscayne Bay, a private dock, dining terrace with barbecue, a walk-in pool with Jacuzzi and waterfall. Villazzo is a hotel operator that transforms private luxury villas into 5-star hotels. Our signature 5-Star VillaHotel Experience includes a lifestyle organized by your own private Hotel Manager and his expert team of hand-picked and uniformed staff whom we train in-house to pamper you.`,
+        description: `Spectacular waterfront villa on Palm Island! This Spanish Colonial style villa features views across Biscayne Bay, a private dock, dining terrace with barbecue, a walk-in pool with Jacuzzi and waterfall. `,
         price: 9999
       },
       {
@@ -68,7 +68,7 @@ module.exports = {
         lat: 23.456453,
         lng: 45.456433,
         name: 'Hidden Spanish Palace',
-        description: `Hidden Spanish palace Is a once in a lifetime gem of an experience! Perfect for weddings, large events, or an unforgettable get away This is a unique Spanish style castle fit for a conquistador. With three extravagant rooms facing nature, Lavish pool Creek hike on the premise, come get lost for a weekend getaway!`,
+        description: `Hidden Spanish palace Is a once in a lifetime gem of an experience! Perfect for weddings, large events, or an unforgettable get away This is a unique Spanish style castle fit for a conquistador. `,
         price: 7000
 
       },
@@ -81,7 +81,7 @@ module.exports = {
         lat: 34.65763,
         lng: 23.35521,
         name: 'Fairy Tale Castle',
-        description: `Smithmore Castle is unlike anywhere in the world. This real life Castle offers exclusive stays curated specifically for your booking. Upon arrival, you will immediately be taken by the magical views of this "Castle in the Sky." Once you've checked in & relaxed in one of our outstanding rooms, a 3 course Gourmet dinner by renowned Chef Benjamin will be served at 6pm in the Royal Dining room. That's not all, your stay will also include a Royal Breakfast served at 8am at our majestic balcony.`,
+        description: `Smithmore Castle is unlike anywhere in the world. This real life Castle offers exclusive stays curated specifically for your booking. `,
         price: 2228
       },
       {
@@ -93,7 +93,7 @@ module.exports = {
         lat: 38.23436,
         lng: 37.35622,
         name: 'The Castle in the Forest',
-        description: "Nestled in a deep canyon nearby serene Lake Arrowhead – you will find Southern Californias secret gem: the Castle In the Forest. Surrounded by towering pine and dogwood trees, this secluded 10,000 square foot chateau provides an unforgettable intimate setting for your stay. It is only 5 minutes away from restaurants in the Lake Arrowhead Village shopping center.",
+        description: "Nestled in a deep canyon nearby serene Lake Arrowhead – you will find Southern Californias secret gem: the Castle In the Forest.",
         price: 3450
       }
     ], { validate: true })
