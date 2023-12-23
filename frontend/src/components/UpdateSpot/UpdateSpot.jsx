d28138b3aebbc2acc044b909e6a13740b5d2f7c1
Muhammad Zubair <mzubair4193@gmail.com>
2023-12-23 15:55:16 -0800

    update spot / seeders updated

--- frontend/src/components/UpdateSpot/UpdateSpot.jsx
+++ frontend/src/components/UpdateSpot/UpdateSpot.jsx
@@ -284,7 +284,7 @@ function UpdateSpot() {
                     ></input>
                 </div>
                 <div className='casButton'>
-                    <button type='submit' className='uas' onClick={validateInputs}>
+                    <button type='submit' className='uas' onClick={handleUpdate}>
                         Update Spot
                     </button>
                 </div>
